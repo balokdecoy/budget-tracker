8e72065d8ea07d839994f69829e3e34a75e9730f
johnniesimpson <johnniesimpson@outlook.com>
2021-03-21 19:12:09 -0400

    Save changes

--- models/transaction.js
+++ models/transaction.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
 
 const transactionSchema = new Schema(
@@ -16,8 +15,11 @@ const transactionSchema = new Schema(
     date: {
       type: Date,
       default: Date.now
-    }
-  }
+    },
+  },
+  {
+    toJSON: {virtuals: true},
+}
 );
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
