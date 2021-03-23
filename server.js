baf16197d18d0dd4ed929620e353a98effe2de8f
johnniesimpson <johnniesimpson@outlook.com>
2021-03-23 18:37:47 -0400

    update server, trying to debug deployment issues.

--- server.js
+++ server.js
@@ -11,14 +11,18 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/budget',
+  process.env.MONGODB_URI || 'mongodb+srv://balokdecoy:Picard5488@cluster0.bunww.mongodb.net/budget?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
     useFindAndModify: false
   }
-);
+).then(() => {
+  console.log('connected')
+}).catch(error => {
+  console.log(error);
+});
 
 // routes
 app.use(require("./routes/api.js"));
